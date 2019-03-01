c18b1b00806849fe4be9cb69359a9045e417c885
benas1989 <laurinaitis.benas@gmail.com>
2019-03-02 00:29:04 +0200

    First test

--- Neopixel/WS2812B.cs
+++ Neopixel/WS2812B.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Spi;
 
 namespace Neopixel
 {
     public class WS2812B
     {
+        private const string SPI_PORT_NAME = "SPI0";
+        private int totalPixels;
+        private byte[] pixelsBuffer;
+        private SpiDevice spi;
+        private bool initialiseComplete = false;
+
+        public void ClearPixelsBuffer()
+        {
+            for (int i = 0; i < totalPixels*3; i++)
+            {
+                pixelsBuffer[i] = 0;
+            }
+        }
+
+        public async Task<bool> InitializeAsync(int pixels)
+        {
+            try
+            {
+                var settings = new SpiConnectionSettings(0);
+                settings.ClockFrequency = 1000000;
+                settings.Mode = SpiMode.Mode1;
+                settings.DataBitLength = 8;
+                string spiAqs = SpiDevice.GetDeviceSelector(SPI_PORT_NAME);
+                var devicesInfo = await DeviceInformation.FindAllAsync(spiAqs);
+                spi = await SpiDevice.FromIdAsync(devicesInfo[0].Id, settings);
+                pixelsBuffer = new byte[pixels * 3];
+                totalPixels = pixels;
+                ClearPixelsBuffer();
+                await ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("InitializeAsync() error: {0}", ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public bool SetPixel(int position, byte red, byte green, byte blue)
+        {
+            if (position >= totalPixels)
+                return false;
+            int startPosition = position * 3;
+            pixelsBuffer[startPosition] = green;
+            pixelsBuffer[startPosition + 1] = red;
+            pixelsBuffer[startPosition + 2] = blue;
+            return true;
+        }
+
+        public async Task ShowAsync()
+        {
+            try
+            {
+                if (!initialiseComplete)
+                    return;
+                spi.Write(pixelsBuffer);
+                await Task.Delay(1);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("ShowAsync() error: {0}", ex.Message);
+            }
+            return;
+        }
+
     }
 }
--- RaspberryPi3_WS2812B/MainPage.xaml.cs
+++ RaspberryPi3_WS2812B/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Neopixel;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,40 @@ namespace RaspberryPi3_WS2812B
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        WS2812B strip;
         public MainPage()
         {
             this.InitializeComponent();
+            Task.Factory.StartNew(() => { Run(); });
+        }
+
+        async void Run()
+        {
+            int pixels = 104;
+            strip = new WS2812B();
+            await strip.InitializeAsync(pixels);
+            while (true)
+            {
+                strip.ClearPixelsBuffer();
+                for (int i = 0; i < pixels; i++)
+                {
+                    strip.SetPixel(i, 100,0,0);
+                    await strip.ShowAsync();
+                }
+                await Task.Delay(500);
+                for (int i = 0; i < pixels; i++)
+                {
+                    strip.SetPixel(i, 0, 100, 0);
+                    await strip.ShowAsync();
+                }
+                await Task.Delay(500);
+                for (int i = 0; i < pixels; i++)
+                {
+                    strip.SetPixel(i, 0, 0, 100);
+                    await strip.ShowAsync();
+                }
+                await Task.Delay(500);
+            }
         }
     }
 }
